c1b8fd7ea19353a236860c5a51e9a04856ef2052
saeki <0ec6ky47385587v@gmail.com>
2023-11-15 12:41:10 +0900

    画像変更

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const mysql = require("mysql2");
 
