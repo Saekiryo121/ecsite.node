eb41123f9b4dece53ae9a3edc440b0ae7f9bcaf8
saeki <0ec6ky47385587v@gmail.com>
2023-11-15 18:43:48 +0900

    一旦セーブ

--- app.js
+++ app.js
@@ -49,4 +49,54 @@ app.get("/edit/:id", (req, res) => {
   });
 });
 
+app.get("/cart", (req, res) => {
+  const sql = "SELECT * FROM cart";
+  con.query(sql, (err, cartItems, fields) => {
+    if (err) throw err;
+
+    let totalPrice = 0;
+    cartItems.forEach(item => {
+      totalPrice += item.price;
+    });
+
+    res.render("cart", { cartItems, totalPrice });
+  });
+});
+
+
+app.post("/add-to-cart", (req, res) => {
+  const newItem = req.body;
+
+  console.log(newItem);
+
+  if (newItem && newItem.menu && newItem.menu.imagePath) {
+    const insertSql = "INSERT INTO cart (imagePath, name, price) VALUES (?, ?, ?)";
+    const insertValues = [newItem.menu.imagePath, newItem.menu.name, newItem.menu.price];
+
+    con.query(insertSql, insertValues, (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.json({ success: false, error: "Failed to add item to cart" });
+      }
+
+      const selectSql = "SELECT * FROM cart";
+      con.query(selectSql, (err, cartItems, fields) => {
+        if (err) throw err;
+
+        let totalPrice = 0;
+        cartItems.forEach(item => {
+          totalPrice += item.price;
+        });
+
+        res.json({ success: true });
+      });
+    });
+  } else {
+    console.error('Item or imagePath not available.');
+    return res.json({ success: false, error: "Item information not available." });
+  }
+});
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
